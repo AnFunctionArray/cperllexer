dfe24abd90380ac407f608097b9632c76be423f9
6a4h8 <sasho648@gmail.com>
2020-12-19 21:11:38 +0200

    Some for

--- maintest.c
+++ maintest.c
@@ -1,12 +1,13 @@
 char b = { .a[4] = 9 * 7, 76 };
 typedef int jk;
 
-jk c=8 * 9;
+jk c = 8 * 9;
 
 int main() {
-	(a[!*&(((char* const(*restrict(*restrict(* const)[1][2])[3])[4])(char const(* const(*restrict * restrict(* const* const)
-		(enum { ab = 9, cb = 20, } x1, struct { int ax : 5, bx[4] : 5; } *restrict b[4])[2][2])[3])[4])sizeof++--"a"++->a7++[*&!7] * 8))]
-		(4[2] + 2 > 6 & *2 << 2 ? a, b : (a = 0) ^ ((char* const)a = 0) ? 1 : 0, 4[2] + 2 > 6 & *2 << 2 ? 2 : (a = 0) ^ ((char* const)a = 0) ? 1 : 0, c));
+	for (int a = 0; a < 5; ++a)
+		(a[!*&(((char* const(*restrict(*restrict(* const)[1][2])[3])[4])(char const(* const(*restrict * restrict(* const* const)
+			(enum { ab = 9, cb = 20, } x1, struct { int ax : 5, bx[4] : 5; } *restrict b[4])[2][2])[3])[4])sizeof++--"a"++->a7++[*&!7] * 8))]
+			(4[2] + 2 > 6 & *2 << 2 ? a, b : (a = 0) ^ ((char* const)a = 0) ? 1 : 0, 4[2] + 2 > 6 & *2 << 2 ? 2 : (a = 0) ^ ((char* const)a = 0) ? 1 : 0, c));
 }
 
-	fkdfk '\'"' "" 0x89 ggfgfg "tes t3" fgdfdf("tes())"("t\4"("\xF9")))r
+fkdfk '\'"' "" 0x89 ggfgfg "tes t3" fgdfdf("tes())"("t\4"("\xF9")))r
