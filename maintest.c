34405d015b88e68ed597aba5a5c3e92529201a06
anikolov <sasho648@gmail.com>
2020-12-25 01:53:09 +0200

    Fixed to match the maintest

--- maintest.c
+++ maintest.c
@@ -1,4 +1,4 @@
-(c?(a):(a=*f=d * 9 + 1 ? 20 : 4 + 6 & 7)^(*(char*const)a=c)+1*7*6);
+
 	
 char b = { .a[4] = 9 * 7, 76 };
 typedef int jk;
@@ -9,7 +9,7 @@ int main() {
 	for (int a = 0; a < 5; ++a)
 		(a[!*&(((char* const(*restrict(*restrict(* const)[1][2])[3])[4])(char const(* const(*restrict * restrict(* const* const)
 			(enum { ab = 9, cb = 20, } x1, struct { int ax : 5, bx[4] : 5; } *restrict b[4])[2][2])[3])[4])sizeof++--"a"++->a7++[*&!7] * 8))]
-			(4[2] + 2 > 6 & *2 << 2 ? a, b : (a = 0 < 5 & 2 * 8 + 4) ^ (*(char* const)a = 0) ? 1 : 0, 4[2] + 2 > 6 & *2 << 2 ? 2 : (a = 0) ^ ((char* const)a = 0) ? 1 : 0, c));
+			(4[2] + 2 > 6 & *2 << 2 ? a, b : (a = 0 < 5 & 2 * 8 + 4) ^ (*(char* const)a = 0) ? 1 : 0, 4[2] + 2 > 6 & *2 << 2 ? 2 : (a = 0) ^ (*(char* const)a = 0) ? 1 : 0, c));
 }
 
 fkdfk '\'"' "" 0x89 ggfgfg "tes t3" fgdfdf("tes())"("t\4"("\xF9")))r
