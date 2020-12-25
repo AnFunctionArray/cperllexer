512660df1a12a120ff2e6a060821174d32e09fe8
anikolov <sasho648@gmail.com>
2020-12-25 02:04:58 +0200

    minor fixups

--- maintest.c
+++ maintest.c
@@ -8,7 +8,7 @@ jk c = 8 * 9;
 int main() {
 	for (int a = 0; a < 5; ++a)
 		(a[!*&(((char* const(*restrict(*restrict(* const)[1][2])[3])[4])(char const(* const(*restrict * restrict(* const* const)
-			(enum { ab = 9, cb = 20, } x1, struct { int ax : 5, bx[4] : 5; } *restrict b[4])[2][2])[3])[4])sizeof++--"a"++->a7++[*&!7] * 8))]
+			(enum b{ ab = 9, cb = 20, } x1, struct a{ int ax : 5, bx[4] : 5; } *restrict b[4])[2][2])[3])[4])sizeof++--"a"++->a7++[*&!7] * 8))]
 			(4[2] + 2 > 6 & *2 << 2 ? a, b : (a = 0 < 5 & 2 * 8 + 4) ^ (*(char* const)a = 0) ? 1 : 0, 4[2] + 2 > 6 & *2 << 2 ? 2 : (a = 0) ^ (*(char* const)a = 0) ? 1 : 0, c));
 }
 
